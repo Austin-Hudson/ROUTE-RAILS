cdd0ec60e5a767450a1faa4a60e329b6beb52beb
Rikki Rabinowitz <rikkirabz@gmail.com>
2016-08-26 17:29:50 -0400

    api call from backend and setting up routes

---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,19 @@
+class RoutesController < ApplicationController
+
+  def index
+    render json: {"Routes Controller": "INDEX route"}
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+# DISTANCE ROUTES - API
 post "/distance" => "distance#trigger_google_distance_api"
+
+#FAVORITE ROUTES
+get "/favorite"         => "routes#index"
+get "/favorite/:name"   => "routes#show"
+post "/favorite/new"    => "routes#create"
+put "/favorite/:id"     => "routes#update"
+delete "/favorite/:id"  => "routes#destroy"
 
 end
