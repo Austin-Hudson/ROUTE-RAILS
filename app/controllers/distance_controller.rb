5ca5f28f82de2ec9dc2a1c1f0f68220d66a49364
Rikki Rabinowitz <rikkirabz@gmail.com>
2016-08-28 17:07:01 -0400

    rails distance controller

--- app/controllers/distance_controller.rb
+++ app/controllers/distance_controller.rb
@@ -2,8 +2,21 @@ class DistanceController < ApplicationController
 
   def trigger_google_distance_api
     startPoint = params[:start]
+    midPoint = params[:mid_locations]
     endPoint = params[:end]
-     data = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + startPoint + '&destinations=' + endPoint + '&key=AIzaSyDJSRrkIFuSWtW8xSBuZUxFVmwoxIAeSJ0')
-     render json: data
+    midString = midPoint.split("").join("|");
+    puts "midpoint is #{midPoint}"
+    puts "midSting is #{midString}"
+     dataBM = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + startPoint + '&destinations=' + midString + '&key=AIzaSyDJSRrkIFuSWtW8xSBuZUxFVmwoxIAeSJ0')
+     #
+     dataMM = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + midString + '&destinations=' + midString + '&key=AIzaSyDJSRrkIFuSWtW8xSBuZUxFVmwoxIAeSJ0')
+
+     dataME = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + midString + '&destinations=' + endPoint + '&key=AIzaSyDJSRrkIFuSWtW8xSBuZUxFVmwoxIAeSJ0')
+
+     render json: {"BM": dataBM,
+                  "MM": dataMM,
+                  "ME": dataME
+                  }
+
   end
 end
