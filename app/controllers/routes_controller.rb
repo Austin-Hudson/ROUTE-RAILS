40f2201216a56159a80cab63541bb895f4f5720c
Austin Hudson <arhudson7@gmail.com>
2016-08-31 17:10:08 -0400

    added some crud functionality

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -11,12 +11,24 @@ class RoutesController < ApplicationController
   end
 
   def create
-    new_route = Route.new(name: params[:name])
+    puts "PPPPPPAAARRRMS", params
+    name = params[:name];
+    shortest_path = params[:shortest_path];
+    shortest_path_str = ""
+
+    shortest_path.each do |el|
+      shortest_path_str += el + "|"
+    end
+
+    shortest_path_str[-1] = ""
+
+    new_route = Route.new(name: params[:name], shortest_path: shortest_path_str)
     if new_route.save
       render json: new_route
     else # otherwise render an error because we messed up.
       render json: {'error': new_route.errors}
     end
+
   end
 
   def update
